c8660afd8fcd51b31cb179ce67a0fd2014231d1a
jimieloranta <82642208+jimieloranta@users.noreply.github.com>
2022-09-13 10:17:12 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useSigninCheck } from "reactfire";
 import Channel from "./components/Channel";
 import SignInOutButton from "./components/SignInOutButton";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   const { status, data: signInCheckResult } = useSigninCheck();
