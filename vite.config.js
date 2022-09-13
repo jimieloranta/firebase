05d0a72fca51dd63d48ad86547a2db7148e97840
jimieloranta <82642208+jimieloranta@users.noreply.github.com>
2022-09-13 12:31:11 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: process.env.CI ? "/firebase-example/" : undefined,
+  base: process.env.CI ? "/firebase/" : undefined,
   server: {
     port: 3000,
   },
