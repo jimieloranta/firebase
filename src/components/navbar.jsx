a7d1a37c9a1ca16bef6d10e6a7a68b264a37897f
jimieloranta <82642208+jimieloranta@users.noreply.github.com>
2022-09-21 09:08:22 +0300

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,21 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import SignInOutButton from "./SignInOutButton";
+import { useUser } from 'reactfire';
 
-function BasicExample() {
+export default function ppbar() {
+  const {data:user} = useUser();
   return (
+    <>
     <Navbar bg="light" expand="sm">
       <Container>
         <Navbar.Brand >Chat</Navbar.Brand>
+        { user && <Navbar.Text>
+          Sisäänkirjautunut : <a>{user.displayName}</a>
+          </Navbar.Text>}
         <SignInOutButton/>
       </Container>
     </Navbar>
+    </>
   );
 }
-
-export default BasicExample;
